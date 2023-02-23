af3e848c3cb0047f8c73664fd530d01ca81edb2b
areyouokayani <aniqahrawat98@hotmail.co.uk>
2023-02-23 13:26:56 +0700

    added class list to images; add fade out effect js

--- Modelling/model.js
+++ Modelling/model.js
@@ -1,23 +1,27 @@
 function carouselImages() {
-    const imageCount = 12;
-    const images = [];
-  
-    for (let i = 0; i < imageCount; i++) {
-      const img = new Image();
-      img.src = `modelImages/img${i + 1}.jpeg`;
-      images.push(img);
-    }
-  
-    let index = Math.floor(Math.random() * 12);
-  
-    const imgElement = document.getElementById('carousel-image');
-  
-    setInterval(() => {
-      index = (index + 1) % images.length;
-      imgElement.src = images[index].src;
-    }, 3000);
+  const imageCount = 20;
+  const images = [];
+  let index = Math.floor(Math.random() * 20);
+
+  for (let i = 0; i < imageCount; i++) {
+    const img = new Image();
+    img.src = `modelImages/img${i + 1}.jpeg`;
+    img.classList.add("carousel-image");
+    images.push(img);
   }
 
- window.addEventListener('load', function(){
-    carouselImages();
- });
+  const imgElement = document.getElementById("carousel-image");
+
+  setInterval(() => {
+    imgElement.classList.add("hidden");
+    setTimeout(() => {
+      imgElement.classList.remove("hidden");
+      index = (index + 1) % images.length;
+      imgElement.src = images[index].src;
+    }, 1000);
+  }, 5000);
+}
+
+window.addEventListener("load", function () {
+  carouselImages();
+});
